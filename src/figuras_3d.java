1bb74e0abeb59a4250481df55bf874381caa0150
Paul Hidalgo <paulesh5@gmail.com>
2023-06-08 00:14:21 -0500

    Creacion menu, area y volumen cubo con private

--- src/figuras_3d.java
+++ src/figuras_3d.java
@@ -1,2 +1,33 @@
 public class figuras_3d {
+    private double lado_cubo, area_cubo, volumen_cubo;
+
+    public figuras_3d(double lado_cubo) {
+        this.lado_cubo = lado_cubo;
+    }
+
+    private double getLado_cubo() {
+        return lado_cubo;
+    }
+
+    public void setLado_cubo(double lado_cubo) {
+        this.lado_cubo = lado_cubo;
+    }
+
+    public double getArea_cubo() {
+        areaCubo();
+        return area_cubo;
+    }
+
+    public double getVolumen_cubo() {
+        volumenCubo();
+        return volumen_cubo;
+    }
+
+    private void areaCubo(){
+        area_cubo = 6 * getLado_cubo() * getLado_cubo();
+    }
+
+    private void volumenCubo(){
+        volumen_cubo = getLado_cubo() * getLado_cubo() * getLado_cubo();
+    }
 }
--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class menuPrincipal {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        figuras_3d cubo = new figuras_3d(0);
 
+        int op;
+        double lado;
+
+        do {
+            menu();
+            System.out.print("Ingresar opción: ");
+            op = sc.nextInt();
+            switch (op){
+                case 1:{
+                    System.out.println("\n\t *** CUBO ***\n");
+                    System.out.print("Ingrese un lado: ");
+                    lado = sc.nextDouble();
+                    cubo.setLado_cubo(lado);
+                    System.out.println("El area del cubo es: " + cubo.getArea_cubo());
+                    System.out.println("El volumen del cubo es: " + cubo.getVolumen_cubo());
+                    break;
+                }
+                case 2:{
+                    System.out.println("\n\t *** FIGURA 2 ***\n");
+                    break;
+                }
+                case 3:{
+                    System.out.println("\n\t *** FIGURA 3 ***\n");
+                    break;
+                }
+                case 4: {
+                    System.out.println("\n\t *** FIGURA 4 ***\n");
+                    break;
+                }
+            }
+        }while (op != 5);
+    }
+
+    private static void menu(){
+        System.out.println("\n\t\t ===== MENU PRINCIPAL =====\n");
+        System.out.println("1. CUBO");
+        System.out.println("2. FIGURA 2");
+        System.out.println("3. FIGURA 3");
+        System.out.println("4. FIGURA 4");
+        System.out.println("5. SALIR");
     }
 }
