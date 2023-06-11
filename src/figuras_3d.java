734bf84e2624f24ed4714352d1299852b45348e5
cristian-simba <davidsimba200@gmail.com>
2023-06-10 19:03:07 -0500

    Area y Volumen de la Esfera

--- src/figuras_3d.java
+++ src/figuras_3d.java
@@ -31,4 +31,110 @@ public class figuras_3d {
         volumen_cubo = getLado_cubo() * getLado_cubo() * getLado_cubo();
     }
 
+    //Cilindro
+    private double altura_cilindro,radio_cilindro,area_cilindro,volumen_cilindro;
+    public figuras_3d(double altura_cilindro, double radio_cilindro) {
+        this.altura_cilindro = altura_cilindro;
+        this.radio_cilindro = radio_cilindro;
+    }
+    public double getAltura_cilindro() { return altura_cilindro; }
+    public double getRadio_cilindro() { return radio_cilindro; }
+    public void setAltura_cilindro(double altura_cilindro) { this.altura_cilindro = altura_cilindro;  }
+    public void setRadio_cilindro(double radio_cilindro) { this.radio_cilindro = radio_cilindro;  }
+    public double getArea_cilindro(){
+        areacilindro();
+        return area_cilindro;
+    }
+    public double getVolumen_cilindro() {
+        volumencilindro();
+        return volumen_cilindro;
+    }
+    private void areacilindro(){
+        area_cilindro = 2*3.14*getRadio_cilindro()*(getRadio_cilindro()+getAltura_cilindro());
+    }
+    private void volumencilindro(){
+        volumen_cilindro = 3.14*(getRadio_cilindro()*getRadio_cilindro())*getAltura_cilindro();
+    }
+
+    //Figura tridimensional Eduardo Almachi
+    private double radio_base_cono, longitud_cono, altura_cono, area_cono, volumen_cono;
+
+    public figuras_3d(double radio_base_cono, double longitud_cono, double altura_cono) {
+        this.radio_base_cono = radio_base_cono;
+        this.longitud_cono = longitud_cono;
+        this.altura_cono = altura_cono;
+    }
+
+    public double getAltura_cono() {
+        return altura_cono;
+    }
+    public void setAltura_cono(double altura_cono) {
+        this.altura_cono = altura_cono;
+    }
+
+    public double getArea_cono() {
+        return area_cono;
+    }
+    public void setArea_cono(double area_cono) {
+        this.area_cono = area_cono;
+    }
+
+    public double getLongitud_cono() {
+        return longitud_cono;
+    }
+
+    public void setLongitud_cono(double longitud_cono) {
+        this.longitud_cono = longitud_cono;
+    }
+
+    public double getVolumen_cono() {
+        return volumen_cono;
+    }
+    public void setVolumen_cono(double volumen_cono) {
+        this.volumen_cono = volumen_cono;
+    }
+
+    public double getAreaCono() {
+        areaCono();
+        return area_cono;
+    }
+
+    public double getVolumenCono() {
+        volumenCono();
+        return volumen_cono;
+    }
+
+    private void areaCono() {
+        area_cono = Math.PI * radio_base_cono * (radio_base_cono * longitud_cono);
+    }
+
+    private void volumenCono() {
+        volumen_cono = 0.33 * Math.PI * (radio_base_cono *radio_base_cono) * altura_cono;
+    }
+
+    // Esfera
+    private double radio_esfera, area_esfera, volumen_esfera;
+
+    public double getRadio_esfera() {
+        return radio_esfera;
+    }
+
+    public void setRadio_esfera(double radio_esfera) {
+        this.radio_esfera = radio_esfera;
+    }
+    public double getAreaEsfera(){
+        areaEsfera();
+        return area_esfera;
+    }
+    public double getVolumenEsfera(){
+        volumenEsfera();
+        return volumen_esfera;
+    }
+    private void areaEsfera(){
+        area_esfera = (4 * Math.PI * (radio_esfera * radio_esfera));
+    }
+    private void volumenEsfera(){
+        volumen_esfera = (4/3 * Math.PI * (radio_esfera * radio_esfera * radio_esfera));
+    }
+
 }
--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -4,9 +4,17 @@ public class menuPrincipal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         figuras_3d cubo = new figuras_3d(0);
+        figuras_3d cilindro = new figuras_3d(0, 0);
+        figuras_3d cono = new figuras_3d(12, 23, 24);
+        figuras_3d esfera = new figuras_3d(0,0,0);
 
         int op;
-        double lado;
+        double lado,altura_C,radio_C;
+
+        //cono
+        double altura_Cono, radio_cono, longitud_cono;
+        //esfera
+        double radio_esfera;
 
         do {
             menu();
@@ -23,15 +31,39 @@ public class menuPrincipal {
                     break;
                 }
                 case 2:{
-                    System.out.println("\n\t *** FIGURA 2 ***\n");
+                    System.out.println("\n\t *** CILINDRO ***\n");
+                    System.out.print("Ingrese la altura: ");
+                    altura_C = sc.nextDouble();
+                    cono.setAltura_cilindro(altura_C);
+                    System.out.print("Ingrese el radio: ");
+                    radio_C = sc.nextDouble();
+                    cilindro.setRadio_cilindro(radio_C);
+                    System.out.println("El area del cilindro es: " + cilindro.getArea_cilindro());
+                    System.out.println("El volumen del cilindro es: " + cilindro.getVolumen_cilindro());
                     break;
                 }
                 case 3:{
-                    System.out.println("\n\t *** FIGURA 3 ***\n");
+                    System.out.println("\n\t *** CONO ***\n");
+                    System.out.print("Ingrese la altura: ");
+                    altura_Cono = sc.nextDouble();
+                    cono.setAltura_cono(altura_Cono);
+                    System.out.print("Ingrese el radio: ");
+                    radio_cono = sc.nextDouble();
+                    cono.setRadio_cilindro(radio_cono);
+                    System.out.print("Ingrese la longitud: ");
+                    longitud_cono = sc.nextDouble();
+                    cono.setLongitud_cono(longitud_cono);
+                    System.out.println("El area del cono es: " + cono.getAreaCono());
+                    System.out.println("El volumen del cono es: " + cono.getVolumenCono());
                     break;
                 }
                 case 4: {
-                    System.out.println("\n\t *** FIGURA 4 ***\n");
+                    System.out.println("\n\t *** Esfera ***\n");
+                    System.out.println("Ingrese el radio: ");
+                    radio_esfera = sc.nextDouble();
+                    esfera.setRadio_esfera(radio_esfera);
+                    System.out.println("El area de la esfera es: " + esfera.getAreaEsfera());
+                    System.out.println("El volumen de la esfera es: "+esfera.getVolumenEsfera());
                     break;
                 }
             }
@@ -41,9 +73,9 @@ public class menuPrincipal {
     private static void menu(){
         System.out.println("\n\t\t ===== MENU PRINCIPAL =====\n");
         System.out.println("1. CUBO");
-        System.out.println("2. FIGURA 2");
-        System.out.println("3. FIGURA 3");
-        System.out.println("4. FIGURA 4");
+        System.out.println("2. CILINDRO");
+        System.out.println("3. CONO");
+        System.out.println("4. ESFERA");
         System.out.println("5. SALIR");
     }
 
