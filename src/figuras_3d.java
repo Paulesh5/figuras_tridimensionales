a2b7952f02c564deb3e3aee62bf4570d48f3f890
iveth-cocha <maria.iveth16@outlook.es>
2023-06-12 20:45:49 -0500

    Documentación

--- src/figuras_3d.java
+++ src/figuras_3d.java
@@ -1,52 +1,60 @@
 public class figuras_3d {
+
+
+    //definir los parametros de la figura cubo
     private double lado_cubo, area_cubo, volumen_cubo;
 
+    /*Constructor para inicializar el parametro lado_cubo, puesto que es fundamental para el calculo de area y volumen del cubo*/
     public figuras_3d(double lado_cubo) {
         this.lado_cubo = lado_cubo;
     }
-
+    //SETTERS and GETTERS del constructor creado
+    //get>>lee, muestra el valor
+    //Set>>asigna un valor
     private double getLado_cubo() {
         return lado_cubo;
     }
-
     public void setLado_cubo(double lado_cubo) {
         this.lado_cubo = lado_cubo;
     }
-
+    //Mostrar area cubo
     public double getArea_cubo() {
         areaCubo();
         return area_cubo;
     }
-
+    //Mostrar Volumen cubo
     public double getVolumen_cubo() {
         volumenCubo();
         return volumen_cubo;
     }
-
+    //Calculo del area del cubo
     private void areaCubo() {
+        //formula para calcular area
         area_cubo = 6 * getLado_cubo() * getLado_cubo();
     }
-
+    //Calculo del Volumen del cubo
     private void volumenCubo() {
+        //formula para calcular el volumen
         volumen_cubo = getLado_cubo() * getLado_cubo() * getLado_cubo();
     }
+//CILINDRO
 
-    //Cilindro
+    //Definir los parametros de la figura cilindro
     private double altura_cilindro, radio_cilindro, area_cilindro, volumen_cilindro;
 
+    //Constructores para los parametros de altura y radio
     public figuras_3d(double altura_cilindro, double radio_cilindro) {
         this.altura_cilindro = altura_cilindro;
         this.radio_cilindro = radio_cilindro;
     }
+    //SETTERS and GETTERS de los constructores creados
 
     public double getAltura_cilindro() {
         return altura_cilindro;
     }
-
     public double getRadio_cilindro() {
         return radio_cilindro;
     }
-
     public void setAltura_cilindro(double altura_cilindro) {
         this.altura_cilindro = altura_cilindro;
     }
@@ -54,38 +62,38 @@ public class figuras_3d {
     public void setRadio_cilindro(double radio_cilindro) {
         this.radio_cilindro = radio_cilindro;
     }
-
+    //Mostrar area cilindro
     public double getArea_cilindro() {
         areacilindro();
         return area_cilindro;
     }
-
+    //Mostrar volumen cilindro
     public double getVolumen_cilindro() {
         volumencilindro();
         return volumen_cilindro;
     }
-
+    //Calculo del area del cilindro
     private void areacilindro() {
         area_cilindro = 2 * 3.14 * getRadio_cilindro() * (getRadio_cilindro() + getAltura_cilindro());
     }
-
+    //Calculo del Volumen del cilindro
     private void volumencilindro() {
         volumen_cilindro = 3.14 * (getRadio_cilindro() * getRadio_cilindro()) * getAltura_cilindro();
     }
-
-    //Figura tridimensional Eduardo Almachi
+//COMO
+    //Definir los parametros de la figura cilindro
     private double radio_base_cono, longitud_cono, altura_cono, area_cono, volumen_cono;
 
+    //Constructores para los parametros de  radio, longitud y altura
     public figuras_3d(double radio_base_cono, double longitud_cono, double altura_cono) {
         this.radio_base_cono = radio_base_cono;
         this.longitud_cono = longitud_cono;
         this.altura_cono = altura_cono;
     }
-
+    //SETTERS and GETTERS de los constructores creados
     public double getAltura_cono() {
         return altura_cono;
     }
-
     public void setAltura_cono(double altura_cono) {
         this.altura_cono = altura_cono;
     }
@@ -113,28 +121,29 @@ public class figuras_3d {
     public void setVolumen_cono(double volumen_cono) {
         this.volumen_cono = volumen_cono;
     }
-
+    //Reorna el valor del area del cono
     public double getAreaCono() {
         areaCono();
         return area_cono;
     }
-
+    //Retorna el valor del volumen del cono
     public double getVolumenCono() {
         volumenCono();
         return volumen_cono;
     }
-
+//Calculo del area del cono
     private void areaCono() {
+        //Math.Pi trabaja con el valor de pi, sin necesidad de definir este valor como uno constante
         area_cono = Math.PI * radio_base_cono * (radio_base_cono * longitud_cono);
     }
-
+    //Calculo del Volumen del cono
     private void volumenCono() {
         volumen_cono = 0.33 * Math.PI * (radio_base_cono * radio_base_cono) * altura_cono;
     }
-
-    // Esfera
+    // ESFERA
+    //Definir los parametros para la esfera
     private double radio_esfera, area_esfera, volumen_esfera;
-
+    ////SETTERS and GETTERS
     public double getRadio_esfera() {
         return radio_esfera;
     }
@@ -142,21 +151,22 @@ public class figuras_3d {
     public void setRadio_esfera(double radio_esfera) {
         this.radio_esfera = radio_esfera;
     }
-
+    //muestra el valor del area del esfera
     public double getAreaEsfera() {
         areaEsfera();
         return area_esfera;
     }
-
+    //muestra el valor del Volumen del esfera
     public double getVolumenEsfera() {
         volumenEsfera();
         return volumen_esfera;
     }
-
+    //Calculo del area de la esfera
     private void areaEsfera() {
+        //Math.Pi trabaja con el valor de pi, sin necesidad de definir este valor como uno constante
         area_esfera = (4 * Math.PI * (radio_esfera * radio_esfera));
     }
-
+    //Calculo del area de la esfera
     private void volumenEsfera() {
         volumen_esfera = (4 / 3 * Math.PI * (radio_esfera * radio_esfera * radio_esfera));
     }
--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
+ //muestra los valores de gran cantidad de decimales, solo con el numero de decimales que se desee
 import java.text.DecimalFormat;
 
 public class menuPrincipal {
     public static void main(String[] args) {
+        //intancias creadas para cada figura
         Scanner sc = new Scanner(System.in);
         figuras_3d cubo = new figuras_3d(0);
         figuras_3d cilindro = new figuras_3d(0, 0);
         figuras_3d cono = new figuras_3d(12, 23, 24);
         figuras_3d esfera = new figuras_3d(0, 0, 0);
-
+    //variable para uso del menu
         int op;
-        double lado, altura_C, radio_C;
-
-        //cono
-        double altura_Cono, radio_cono, longitud_cono;
-        //esfera
-        double radio_esfera;
+        //variables para ingreso de valores de los constructores creados de las figuras
+        double lado, altura_C, radio_C;//cubo
+        double altura_Cono, radio_cono, longitud_cono;//cono
+        double radio_esfera;//esfera
 
         do {
             menu();
@@ -25,29 +25,34 @@ public class menuPrincipal {
                 case 1: {
                     System.out.println("\n\t *** CUBO ***\n");
                     System.out.print("Ingrese un lado: ");
+                    //Ingreso del valor del lado del cubo
                     lado = sc.nextDouble();
                     cubo.setLado_cubo(lado);
-                    DecimalFormat formato = new DecimalFormat("#.##");
-                    System.out.println("El area del cubo es: " + formato.format(cubo.getArea_cubo()));
-                    System.out.println("El volumen del cubo es: " + formato.format(cubo.getVolumen_cubo()));
+                    //establece el numero de decimales que se muesta del resultado de area y volumen
+                    DecimalFormat formato = new DecimalFormat("#.##");//solo dos decimales
+                    System.out.println("El area del cubo es: " + formato.format(cubo.getArea_cubo()));//muestra area
+                    System.out.println("El volumen del cubo es: " + formato.format(cubo.getVolumen_cubo()));//muestra volumen
                     break;
                 }
                 case 2: {
                     System.out.println("\n\t *** CILINDRO ***\n");
                     System.out.print("Ingrese la altura: ");
+                    //Ingreso del valor de altura y radio del cilindro
                     altura_C = sc.nextDouble();
                     cilindro.setAltura_cilindro(altura_C);
                     System.out.print("Ingrese el radio: ");
                     radio_C = sc.nextDouble();
                     cilindro.setRadio_cilindro(radio_C);
+                    //establece el numero de decimales que se muesta del resultado de area y volumen
                     DecimalFormat formato = new DecimalFormat("#.##");
-                    System.out.println("El area del cilindro es: " + formato.format(cilindro.getArea_cilindro()));
-                    System.out.println("El volumen del cilindro es: " + formato.format(cilindro.getVolumen_cilindro()));
+                    System.out.println("El area del cilindro es: " + formato.format(cilindro.getArea_cilindro()));//muestra area
+                    System.out.println("El volumen del cilindro es: " + formato.format(cilindro.getVolumen_cilindro()));//muestra volumen
                     break;
                 }
                 case 3: {
                     System.out.println("\n\t *** CONO ***\n");
                     System.out.print("Ingrese la altura: ");
+                    //Ingreso del valor de altura, radio y longitud del cono
                     altura_Cono = sc.nextDouble();
                     cono.setAltura_cono(altura_Cono);
                     System.out.print("Ingrese el radio: ");
@@ -56,19 +61,20 @@ public class menuPrincipal {
                     System.out.print("Ingrese la longitud: ");
                     longitud_cono = sc.nextDouble();
                     cono.setLongitud_cono(longitud_cono);
-                    DecimalFormat formato = new DecimalFormat("#.##");
-                    System.out.println("El area del cono es: " + formato.format(cono.getAreaCono()));
-                    System.out.println("El volumen del cono es: " + formato.format(cono.getVolumenCono()));
+                    DecimalFormat formato = new DecimalFormat("#.##");//uso de solo dos decimales
+                    System.out.println("El area del cono es: " + formato.format(cono.getAreaCono()));//muestra area
+                    System.out.println("El volumen del cono es: " + formato.format(cono.getVolumenCono()));//muestra volumen
                     break;
                 }
                 case 4: {
                     System.out.println("\n\t *** Esfera ***\n");
                     System.out.print("Ingrese el radio: ");
+                    //Ingreso del valor de radio de la esfera
                     radio_esfera = sc.nextDouble();
                     esfera.setRadio_esfera(radio_esfera);
                     DecimalFormat formato = new DecimalFormat("#.##");
-                    System.out.println("El area de la esfera es: " + formato.format(esfera.getAreaEsfera()));
-                    System.out.println("El volumen de la esfera es: " + formato.format(esfera.getVolumenEsfera()));
+                    System.out.println("El area de la esfera es: " + formato.format(esfera.getAreaEsfera()));//muestra area
+                    System.out.println("El volumen de la esfera es: " + formato.format(esfera.getVolumenEsfera()));//muestra volumen
                     break;
                 }
             }
